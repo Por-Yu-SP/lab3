ce3e05b87f0ea8b6593610f7aa9d9649b5a6d58e
PorYu <yu07.24@ichat.sp.edu.sg>
2025-10-29 20:55:26 +0800

    added calc avg salary func

--- employee_info.py
+++ employee_info.py
@@ -23,8 +23,9 @@ def calculate_average_salary():
     average = 0
 
     #add your implementation to calculate here
-
-
+    for item in employee_data:
+        total+=item["salary"]
+    average=total/len(employee_data)
     return average
 
 def get_employees_by_dept(department):
